8ae2c297821d744f0897f4b720eed9c469f60bac
Zeus <andres.felipe.florez62@gmail.com>
2024-02-16 15:31:27 -0500

    adding traceroute bandwith and nmap testing to the final code

--- networkScript.sh
+++ networkScript.sh
@@ -10,24 +10,46 @@ echo "1. Chechinh Internet  Connectvity"
 
 ping -c 4 www.google.com
 
-echo "........."
+echo "---------"
 
 # Display Network Interfaces and Ip Addresses
 
 echo "2 Displaying Network Interfaces and ip Addresses'
 ipconfig
-echo "............"
+echo "----------"
 
 # Display Routing table 
 
 echo "3. Displaying Routing Table"
 netstat -rn 
-echo ".........."
+echo "-----------"
 
 # Dns resolution 
 
 echo "4. Dns Resolution"
 nslookup wwww.google.com.co
 
-echo ".........."
+echo "------------"
 
+# Check open ports on Local Machine
+read -p "Enter the Ip Address or hostname to check open ports on : "remote _host
+echo "Checking Open Ports on $remote_host"
+nmap -p $remote_host
+echo "------------"
+
+# Traceroute to a remote Host 
+read -p " Enter the Ip address or hostname to trace route to: " trace_host
+echo "Trace route to: $trace_host"
+tracert $trace_host
+echo "-------------"
+
+#Test Banwidth  using Speed test Cli for Windows
+read -p "Do you want to test nbandwith with speedtest Cli? (y/n): "speedtest_option
+
+if ["speedtest_option" == "y"]; then 
+.speedtestbyookia_x64
+
+fi
+echo "----------"
+
+echo "Network testing Completed"
