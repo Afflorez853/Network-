e8f1fb0391dbae87fae029b2032aa0a8ffb96608
Zeus <andres.felipe.florez62@gmail.com>
2024-02-16 14:42:15 -0500

    Build a Feature to Check Internet Connectivity

---
+++ networkScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Network testing script for git bash on Windows
+
+echo "Network Testing Script"
+
+#Check Internet Connectivity
+
+echo "1. Chechinh Internet  Connectvity"
+
+ping -c 4 www.google.com
+
+echo "........."
